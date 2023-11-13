9c25345f53ace21f31fca423ff2c53fd0888c61f
Rostislav Babáček <rostislav.babacek@ackee.cz>
2023-11-13 18:24:30 +0100

    🚛 Create 08_MVVM folder

---
+++ sources/08_MVVM/FITstagram/FITstagram/FITstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  FITstagramApp.swift
+//  FITstagram
+//
+//  Created by Rostislav Babáček on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FITstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
